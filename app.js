004e477ef5a96411f1b534cbb8e7835b48831213
NoorSim <nurafni.simamora@gmail.com>
2025-05-05 16:44:30 +0300

    Functions

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-for (let i = 1; i <= 20; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-    console.log(`${i} -> Frontend Simplified`)
-  } 
-  else if (i % 3 === 0) {
-    console.log(`${i} -> Frontend`)
-  } 
-  else if (i % 5 === 0) {
-    console.log(`${i} -> Simplified`)
-  } 
-  else {
-    console.log(`${i} -> ${i}`)
-  }
+function convertCelsiusToFarenheit (celsius) {
+return celsius * 1.8 + 32
 }
+
+console.log(convertCelsiusToFarenheit(0))
+console.log(convertCelsiusToFarenheit(10))
+console.log(convertCelsiusToFarenheit(30))
