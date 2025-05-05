2d038247adab0440ef8b5d5006df62cfc3469187
NoorSim <nurafni.simamora@gmail.com>
2025-05-05 09:28:04 +0300

    for loops example

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-let cash = 50
-let price = 40
-let isStoreOpen = true
-
-let str = cash >= price && isStoreOpen? 'give receipt' : 'do not give receipt'
-console.log(str);
+for (let i = 1; i <= 20; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} -> Frontend Simplified`)
+  } 
+  else if (i % 3 === 0) {
+    console.log(`${i} -> Frontend`)
+  } 
+  else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+  } 
+  else {
+    console.log(`${i} -> ${i}`)
+  }
+}
