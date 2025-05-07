3ed2e5346e6b57a9767ad2cffd36743c8a4ef179
NoorSim <nurafni.simamora@gmail.com>
2025-05-07 13:47:59 +0300

    example without array filter method

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
+//Filter out 'FAIL' without using the array filter method
 let grades = ['A+', 'A', 'FAIL']
 
-let goodGrades = grades.filter (element => element !== 'FAIL')
+let goodGrades = []
+
+for (let i = 0; i < grades.length; ++i) {
+  if (grades[i] !== 'FAIL') {
+    goodGrades.push(grades[i])
+  }
+}
 
 console.log(goodGrades)
